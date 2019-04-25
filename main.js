3dcc9afa0e24e528f925e7f109177cd1fc170f24
Eden Turgeman <edmx0.et@gmail.com>
2019-04-25 12:16:26 +0300

    feat(title-bar): the app is now a frameless window with a fully functioning custom title bar

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const { app, BrowserWindow} = require('electron');
+const {app, BrowserWindow} = require('electron');
 require('./src/electron/exif-events');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 600, height: 600 });
+function createWindow() {
+    win = new BrowserWindow({width: 400, height: 400, frame: false});
     win.setMenuBarVisibility(false);
-    // and load the index.html of the app.
+
     win.loadURL('http://localhost:3000');
 
-    // Open the DevTools.
     win.webContents.openDevTools()
 
     // Emitted when the window is closed.
--- src/TitleBar/TitleBar.js
+++ src/TitleBar/TitleBar.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import {Close, CropSquareOutlined, MinimizeOutlined} from '@material-ui/icons'
 import {withStyles, IconButton} from "@material-ui/core";
 
+const {remote} = window.require('electron');
+
+// A bit of a mess style wise because the special electron properties don't always work, same with background-color and Iconbutton
 const StyledTitleBar = styled.div`
     position: fixed;
     display: flex;
@@ -10,32 +13,58 @@ const StyledTitleBar = styled.div`
     flex-flow: row;
     top: 0;
     width: 100%;
-    height: 25px;
+    -webkit-user-select: none;
+    -webkit-app-region: drag;
+    
+`;
+
+const StyledIconButton = styled(IconButton)`
+    -webkit-app-region: no-drag;
 `;
 
 const styles = theme => ({
     button: {
         "&:hover": {
-            backgroundColor: "transparent",
-            fontSize: '5px'
+            backgroundColor: "transparent"
         }
+    },
+    icon: {
+        fontSize: "20px"
     }
 });
 
 
 const TitleBar = props => {
     const {classes} = props;
+
+    const minimizeWindow = () => {
+        remote.getCurrentWindow().minimize()
+    };
+
+    const minMaxWindow = () => {
+        const currentWindow = remote.getCurrentWindow();
+
+        currentWindow.isMaximized() ? currentWindow.unmaximize() : currentWindow.maximize()
+    };
+
+    const closeWindow = () => {
+        remote.app.quit();
+    };
+
     return (
-        <StyledTitleBar>
-            <IconButton className={classes.button} disableFocusRipple={true} disableRipple={true}>
-                <MinimizeOutlined/>
-            </IconButton>
-            <IconButton className={classes.button} disableFocusRipple={true} disableRipple={true}>
-                <CropSquareOutlined/>
-            </IconButton>
-            <IconButton className={classes.button} disableFocusRipple={true} disableRipple={true}>
-                <Close/>
-            </IconButton>
+        <StyledTitleBar className="drag">
+            <StyledIconButton className={classes.button} onClick={minimizeWindow} disableFocusRipple={true}
+                              disableRipple={true}>
+                <MinimizeOutlined className={classes.icon}/>
+            </StyledIconButton>
+            <StyledIconButton onClick={minMaxWindow} className={classes.button} disableFocusRipple={true}
+                              disableRipple={true}>
+                <CropSquareOutlined className={classes.icon}/>
+            </StyledIconButton>
+            <StyledIconButton onClick={closeWindow} className={classes.button} disableFocusRipple={true}
+                              disableRipple={true}>
+                <Close className={classes.icon}/>
+            </StyledIconButton>
         </StyledTitleBar>
     );
 };
